3a415016f31a3a4699d3bf9d13a316259f89754d
George Mihov <gmihov001@gmail.com>
2022-01-11 19:51:18 +0000

    Completed 73

--- exercises/073-countNumberOfKeys/app.js
+++ exercises/073-countNumberOfKeys/app.js
@@ -1,4 +1,13 @@
 function countNumberOfKeys(obj) {
-    // your code here
-
+    var size = Object.keys(obj).length;
+    return size;
 }
+
+var obj = {
+    a: 1,
+    b: 2,
+    c: 3
+  };
+
+var output = countNumberOfKeys(obj);
+console.log(output);
