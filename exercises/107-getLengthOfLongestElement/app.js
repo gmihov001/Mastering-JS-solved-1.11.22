23b0c2df084fadf7c608f9170aa64f3704a5c553
George Mihov <gmihov001@gmail.com>
2022-01-11 20:47:37 +0000

    Completed 107

--- exercises/107-getLengthOfLongestElement/app.js
+++ exercises/107-getLengthOfLongestElement/app.js
@@ -1,6 +1,14 @@
 function getLengthOfLongestElement(arr) {
-    // your code here
+    let longest = '';
+    if(arr.length == 0) return 0;
+    else {
+        arr.forEach(word => {
+            if(word.length > longest.length) longest = word;
+        });    
+        return longest.length;
+    }
+    
 }
 
 var output = getLengthOfLongestElement(['one', 'two', 'three']);
-console.log(output); // --> 5
+console.log(output); 
