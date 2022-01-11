12d2b8a2c53dbfa1178112d1ccdec93eed042a33
George Mihov <gmihov001@gmail.com>
2022-01-11 20:40:56 +0000

    Completed 97

--- exercises/097-keep/app.js
+++ exercises/097-keep/app.js
@@ -1,1 +1,7 @@
 // Write your function here
+const keep = (arr, item) => {
+    let toKeep = arr.filter((element) => element == item);
+}
+
+var output = keep([1, 2, 3, 2, 1], 2) 
+console.log(output); 
