411cce7219e9f97e7e834d943e1ca21200e529ac
George Mihov <gmihov001@gmail.com>
2022-01-11 17:54:51 +0000

    Completed 06

--- exercises/005-getProperty/app.js
+++ exercises/005-getProperty/app.js
@@ -1,3 +1,10 @@
 function getProperty(obj, key) {
-  // your code here
+  return obj[key] ? obj[key] : undefined;
 }
+
+var car = {
+  model: 'Toyota'
+};
+
+var output = getProperty(car, 'model');
+console.log(output);
