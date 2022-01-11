16bc7de41d47bea47649626ab65f6c9380148f25
George Mihov <gmihov001@gmail.com>
2022-01-11 19:18:53 +0000

    Completed 66

--- exercises/066-isEitherEvenOrAreBoth7/app.js
+++ exercises/066-isEitherEvenOrAreBoth7/app.js
@@ -1,3 +1,12 @@
 function isEitherEvenOrAreBoth7(num1, num2) {
-    // your code here
+    let even = num1 % 2 == 0 || num2 % 2 == 0;
+    let seven = num1 == 7 && num2 == 7;
+    return even || seven;
 }
+
+
+var output = isEitherEvenOrAreBoth7(3, 7);
+console.log(output); // --> false
+
+var output = isEitherEvenOrAreBoth7(2, 3);
+console.log(output);
