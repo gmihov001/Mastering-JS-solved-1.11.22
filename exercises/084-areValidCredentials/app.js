9c1793162540a3a4a78339278c3f103c43ccffb6
George Mihov <gmihov001@gmail.com>
2022-01-11 20:31:33 +0000

    Completed 84

--- exercises/084-areValidCredentials/app.js
+++ exercises/084-areValidCredentials/app.js
@@ -1,1 +1,5 @@
 // Write your function here
+const areValidCredentials = (name, pw) => name.length > 3 && pw.length >= 8;
+
+var output = areValidCredentials('Ritu', 'mylongpassword')
+console.log(output);
