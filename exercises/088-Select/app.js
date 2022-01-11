097e28729acfeb7e66e239344f9cb741e6898e5b
George Mihov <gmihov001@gmail.com>
2022-01-11 20:36:50 +0000

    Completed 88

--- exercises/088-Select/app.js
+++ exercises/088-Select/app.js
@@ -1,1 +1,21 @@
 // Write your function here
+
+const select = (arr, obj) => {
+    let newObj = {};
+    arr.forEach((key)=>{
+        if(key in obj) newObj[key] = obj[key];
+    });
+
+    return newObj;
+}
+
+var arr = ['a', 'c', 'e'];
+var obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4
+};
+
+var output = select(arr, obj);
+console.log(output); 
