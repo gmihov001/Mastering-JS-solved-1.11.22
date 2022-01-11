7fd7c875168086559d25e10b1e27174ebdccf95b
George Mihov <gmihov001@gmail.com>
2022-01-11 18:05:26 +0000

    Completed 11

--- exercises/011-getLengthOfTwoWords/app.js
+++ exercises/011-getLengthOfTwoWords/app.js
@@ -1,3 +1,6 @@
 function getLengthOfTwoWords(word1, word2) {
-  // your code here
+  return word1.concat(word2).length;
 }
+
+var output = getLengthOfTwoWords('some', 'words');
+console.log(output);
