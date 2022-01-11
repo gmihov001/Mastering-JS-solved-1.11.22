cd3da4be91b8f00a6e642b09348c78ac373afa15
George Mihov <gmihov001@gmail.com>
2022-01-11 20:29:08 +0000

    Completed 80

--- exercises/080-getAverageOfElementsAtProperty/app.js
+++ exercises/080-getAverageOfElementsAtProperty/app.js
@@ -1,1 +1,14 @@
 // Write your function here
+const getAverageOfElementsAtProperty = (obj, key) => {
+  if(!obj[key] || obj[key].length == 0) return 0;
+  else if(!(key in obj)) return 0; 
+  else if(!Array.isArray(obj[key])) return 0;
+  else return obj[key].reduce((a, b) => a + b) / obj[key].length;
+}
+
+var obj = {
+    key: [1, 2, 3]
+  };
+
+  var output = getAverageOfElementsAtProperty(obj, 'key');
+  console.log(output); 
