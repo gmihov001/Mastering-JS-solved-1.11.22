3eabc3a0dfb367a1a535c755374ffeeb85cce98f
George Mihov <gmihov001@gmail.com>
2022-01-12 00:47:46 +0000

    Completed 140

--- exercises/140-modulo/app.js
+++ exercises/140-modulo/app.js
@@ -1,6 +1,14 @@
 function modulo(num1, num2) {
-    // your code here
+    
+    let division = num1 / num2;
+    let floored = Math.floor(division);
+    let withoutRemainder = floored * num2;
+    let remainder = num1 - withoutRemainder;
+    if(Math.sign(num1) == -1) return -Math.abs(remainder);
+    else return remainder;
 }
 
 var output = modulo(25, 4);
 console.log(output); // --> 1
+
+console.log(100 % 38); //to cross check the results
