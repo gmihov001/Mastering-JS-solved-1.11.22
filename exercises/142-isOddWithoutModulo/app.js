6f66a1126e16cd09a87e2cc891c80abaae100099
George Mihov <gmihov001@gmail.com>
2022-01-12 21:47:38 +0000

    Completed 142

--- exercises/142-isOddWithoutModulo/app.js
+++ exercises/142-isOddWithoutModulo/app.js
@@ -1,5 +1,10 @@
 function isOddWithoutModulo(num) {
-    // your code here
+    let division = num / 2; //divide number by 2
+    let floored = Math.floor(division); //remove any remainder
+
+    if(floored * 2 != num) return true; //if number without remainder is different from original number, then it must be odd
+    else return false;
 }
+
 var output = isOddWithoutModulo(17);
 console.log(output); // --> true
