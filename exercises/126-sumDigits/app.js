ea95ed2ffacf135eeeecdfcba68a20e2e38dc280
George Mihov <gmihov001@gmail.com>
2022-01-12 00:07:01 +0000

    Completed 126

--- exercises/126-sumDigits/app.js
+++ exercises/126-sumDigits/app.js
@@ -1,5 +1,13 @@
 function sumDigits(num) {
-    // your code here
+    let digits = num.toString();
+    let sum = 0;
+    for(let i = 0; i < digits.length; i++) {
+        if(i == 0 && digits.charAt(i) == '-' ) sum += 0;
+        else if(i-1 == 0 && digits.charAt(i-1) == '-') sum -= parseInt(digits.charAt(i));
+        else sum += parseInt(digits.charAt(i));
+    }
+    return sum;
 }
-var output = sumDigits(316);
-console.log(output); // --> 4
+
+var output = sumDigits(-316);
+console.log(output); 
