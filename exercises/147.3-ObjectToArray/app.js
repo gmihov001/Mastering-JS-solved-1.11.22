2f10dda812ad3b2185e02379408eab17cb1d9898
George Mihov <gmihov001@gmail.com>
2022-01-12 22:33:51 +0000

    Completed 147.3

--- exercises/147.3-ObjectToArray/app.js
+++ exercises/147.3-ObjectToArray/app.js
@@ -1,3 +1,16 @@
-function convertObjectToList(obj) {
-  // your code here
+function convertObjectToList(object) {
+  let newArr = [];
+  for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+      const element = [key, object[key]];
+      newArr.push(element);      
+    }
+  }
+  return newArr;
 }
+
+console.log(convertObjectToList({
+  name: 'Holly',
+  age: 35,
+  role: 'producer'
+}));
