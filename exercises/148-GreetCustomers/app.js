7f72137eed89bcfee4d62f0c7fe71b5b00c71848
George Mihov <gmihov001@gmail.com>
2022-01-12 22:52:54 +0000

    Completed 148

--- exercises/148-GreetCustomers/app.js
+++ exercises/148-GreetCustomers/app.js
@@ -16,7 +16,25 @@ var customerData = {
 function greetCustomer(firstName) {
   var greeting = '';
 	
-  // your code here
+  if(!(firstName in customerData)) {
+    greeting = 'Welcome! Is this your first time?';
+  } else {
+    if(customerData[firstName].visits == 1) {
+      greeting = `Welcome back, ${firstName}! We're glad you liked us the first time!`;
+    } else if(customerData[firstName].visits > 1) {
+      greeting = `Welcome back, ${firstName}! So glad to see you again!`;
+    }  
+    
+  }
 	
   return greeting;
 }
+
+var output = greetCustomer('Terrance');
+console.log(output);
+
+var output = greetCustomer('Joe');
+console.log(output);
+
+var output = greetCustomer('Carol');
+console.log(output);
