337f3490cb1bebc32b4602667399d075d5d92fb1
George Mihov <gmihov001@gmail.com>
2022-01-12 21:55:46 +0000

    Completed 146.1

--- exercises/146.1-ArrayToObject/app.js
+++ exercises/146.1-ArrayToObject/app.js
@@ -1,3 +1,8 @@
 function transformFirstAndLast(array) {
-  // your code here
+  let obj = {};
+  obj[array[0]] = array[array.length-1];
+  return obj;
 }
+
+let output = transformFirstAndLast(['Kevin', 'Bacon', 'Love', 'Hart', 'Costner', 'Spacey']);
+console.log(output);
