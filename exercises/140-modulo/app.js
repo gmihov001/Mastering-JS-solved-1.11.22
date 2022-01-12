e71ff45cdbdb9fff03d6a1f7dff6d74b40355b76
George Mihov <gmihov001@gmail.com>
2022-01-12 21:21:06 +0000

    Completed 140

--- exercises/140-modulo/app.js
+++ exercises/140-modulo/app.js
@@ -1,14 +1,18 @@
 function modulo(num1, num2) {
-    
+    let isNeg = false; //tracks if num1 is negative or positive
+    if(Math.sign(num1) == -1) isNeg = true; //if num1 is neg, isNeg is true
+    if(isNeg) num1 = Math.abs(num1); //if num1 is neg, convert to absolute (positive) 
+
     let division = num1 / num2;
     let floored = Math.floor(division);
     let withoutRemainder = floored * num2;
     let remainder = num1 - withoutRemainder;
-    if(Math.sign(num1) == -1) return -Math.abs(remainder);
+    
+    if(isNeg) return -Math.abs(remainder); //because num1 was negative, convert remainder to negative
     else return remainder;
 }
 
-var output = modulo(25, 4);
-console.log(output); // --> 1
+var output = modulo(-100, 38);
+console.log(output); 
 
 console.log(100 % 38); //to cross check the results
