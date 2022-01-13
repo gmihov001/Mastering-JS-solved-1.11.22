99b66456a0bb410deea3b2ce2dd89feed00fd606
George Mihov <gmihov001@gmail.com>
2022-01-13 16:48:25 +0000

    Completed 153

--- exercises/153-isRotated/app.js
+++ exercises/153-isRotated/app.js
@@ -1,5 +1,10 @@
 function isRotated(str1, str2) {
-    // your code here
+    let notInclude = 0;
+    for (let index = 0; index < str1.length; index++) {
+        if(!(str2.includes(str1.charAt(index)))) notInclude += 1;     
+    }
+    if(notInclude > 0) return false;
+    else return true;
 }
 
 console.log(isRotated('hello world', 'orldhello w')) // => true
