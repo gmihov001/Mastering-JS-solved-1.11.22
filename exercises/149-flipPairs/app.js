7cddeb6b5a1cd0049c669e986acf758477652a17
George Mihov <gmihov001@gmail.com>
2022-01-13 15:17:18 +0000

    Completed 149

--- exercises/149-flipPairs/app.js
+++ exercises/149-flipPairs/app.js
@@ -1,5 +1,18 @@
 function flipPairs(input){
-    // your code here
+    let newStr = '';
+    if(input.length % 2 == 0) {
+        for (let index = 1; index < input.length; index = index + 2) {
+            newStr += (input.charAt(index) + input.charAt(index-1));
+            
+        }
+    } else {
+        for (let index = 1; index < input.length; index = index + 2) {
+            newStr += (input.charAt(index) + input.charAt(index-1));
+            
+        }
+        newStr += input.charAt(input.length-1);
+    }    
+    return newStr;
 }
 
 var input = 'check out how interesting this problem is, it\'s insanely interesting!';
