0c7849303b12f4017795918849c189f82ccd79fb
George Mihov <gmihov001@gmail.com>
2022-01-14 15:42:35 +0000

    Completed 156

--- exercises/156-isogram/app.js
+++ exercises/156-isogram/app.js
@@ -1,9 +1,11 @@
 function isIsogram(text) {
   // add each char to a set
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
-  // note: a set drops dup values
-  // thus, to see if all the chars were unique,
-  // check length of text and the size of the set 
+  const mySet1 = new Set();
+  for (const iterator of text) {
+    mySet1.add(iterator);      
+  }
+  
+  return(mySet1.size == text.length);
 }
 
 console.log(isIsogram("Camile")) // => true
