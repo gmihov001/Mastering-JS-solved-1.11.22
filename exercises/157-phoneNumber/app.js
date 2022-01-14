243a21f8d6427ed4a2f8fe290f9f88d12117150f
George Mihov <gmihov001@gmail.com>
2022-01-14 17:09:38 +0000

    Completed 157

--- exercises/157-phoneNumber/app.js
+++ exercises/157-phoneNumber/app.js
@@ -4,20 +4,20 @@ function PhoneNumberFormatter(numbers) {
 
 PhoneNumberFormatter.prototype.render = function() {
   var string = '';
-  // your code here
+  string = this.getAreaCode() + ' ' + this.getExchangeCode() + "-" + this.getLineNumber();
   return string;
 };
 
 PhoneNumberFormatter.prototype.getAreaCode = function() {
-  // your code here
+  return this.parenthesize(this.slice(0,3)); 
 };
 
 PhoneNumberFormatter.prototype.getExchangeCode = function() {
-  // your code here
+  return this.slice(3,6);
 };
 
 PhoneNumberFormatter.prototype.getLineNumber = function() {
-  // your code here
+  return this.slice(6,10);
 };
 
 PhoneNumberFormatter.prototype.parenthesize = function(string) {
@@ -25,5 +25,7 @@ PhoneNumberFormatter.prototype.parenthesize = function(string) {
 };
 
 PhoneNumberFormatter.prototype.slice = function(start, end) {
-  return this.numbers.slice(start, end).join('');
+  return("slice", this.numbers.slice(start, end).join(''));
 };
+
+console.log(new PhoneNumberFormatter([6, 5, 0, 8, 3, 5, 9, 1, 7, 2]).render());
